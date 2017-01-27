4da9a49a9957327620cd6c7f91717a3f0437c6d3
joshkmcdonald <joshkmcdonald@gmail.com>
2017-01-27 09:26:58 -0700

    Complete jan 23 exercise

--- january/23.rb
+++ january/23.rb
@@ -1,4 +1,14 @@
+# done
 require 'rspec'
+
+class User
+  attr_accessor :name, :email
+
+  def initialize
+    yield(self) if block_given?
+  end
+
+end
 
 describe 'Block User' do
   it 'Allows a user to be created with a block of values' do
