0af34eedb8c52066fb23fb550b736bfdb6c97139
joshkmcdonald <joshkmcdonald@gmail.com>
2017-01-31 09:53:26 -0700

    complete Jan 28 challenge. Subtracting dates and returning an integer

---
+++ january/28.rb
@@ -0,0 +1,13 @@
+# done
+require 'rspec'
+require 'date'
+
+def day_counter
+  (Date.new(2016,12,25) - Date.new(2004,7,1)).to_i
+end
+
+describe 'Day counter' do
+  it 'counts the days between Christmas in 2016 and July 1, 2004' do
+    expect(day_counter).to eq(4560)
+  end
+end
