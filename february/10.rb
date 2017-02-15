032c9ead74bfe397c3f5203023795b9d9e184e6d
joshkmcdonald <joshkmcdonald@gmail.com>
2017-02-15 10:19:11 -0700

    Complete invoice Struct sorter

---
+++ february/10.rb
@@ -0,0 +1,20 @@
+# done
+require 'rspec'
+
+def total_sorter invoices
+  invoices.sort_by{ | e | -e.total }
+end
+
+describe 'Invoice sorter' do
+  it 'sorts a set of invoices by their totals' do
+    Invoice = Struct.new(:name, :total, :category)
+    google = Invoice.new('Google', 500, 'Marketing')
+    amazon = Invoice.new('Amazon', 1000, 'eCommerce')
+    yahoo = Invoice.new('Yahoo', 300, 'Marketing')
+
+    invoices = [google, amazon, yahoo]
+
+    expect(total_sorter(invoices).first.name).to eq('Amazon')
+    expect(total_sorter(invoices).last.name).to eq('Yahoo')
+  end
+end
